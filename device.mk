4075c40a3ba18f9423dd32a5bd4590a28ec2c691
masemoel <masemoel@users.noreply.github.com>
2024-08-11 11:50:13 +0000

    phoenix: overlays: Convert NotchNoFillOverlay to RRO_Overlay

--- device.mk
+++ device.mk
@@ -377,10 +377,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_phoenix/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_phoenix/com.nxp.mifare.xml
 
-# Notch style overlay
-PRODUCT_PACKAGES += \
-    NotchNoFillOverlay
-
 # Overlays
 PRODUCT_PACKAGES += \
     CarrierConfigOverlayPhoenix \
@@ -391,6 +387,7 @@ PRODUCT_PACKAGES += \
     SystemUIOverlayPhoenix \
     TelephonyOverlayPhoenix \
     TetheringConfigOverlayPhoenix \
+    NotchNoFillOverlayPhoenix \
     WifiOverlayPhoenix
 
 # Perf
