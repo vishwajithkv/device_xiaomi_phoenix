d40b86c960f6f7bb9736fc71533768cf655129ba
vishwajithkv <vishwajithkv@users.noreply.github.com>
2024-08-21 22:01:21 +0530

    phoenix: RisingOS bringup

--- lineage_phoenix.mk
+++ lineage_phoenix.mk
@@ -11,17 +11,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from phoenix device
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
-# Inherit some common Pixel-Project stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common risingOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-#Flags for Pixel-Project
-TARGET_BOOT_ANIMATION_RES := 1080
-USE_PIXEL_CHARGER = true
-TARGET_INCLUDE_LIVE_WALLPAPERS=true
-TARGET_SUPPORTS_GOOGLE_RECORDER=true
-TARGET_FACE_UNLOCK_SUPPORTED := true
+# RisingOS
+RISING_MAINTAINER := leoxvis(vishwajithkv)
+RISING_CHIPSET := Snapdragon730G
+PRODUCT_NO_CAMERA := false
+TARGET_ENABLE_BLUR := true
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_MAINTAINER="leoxvis(vishwajithkv)"
 
-PRODUCT_NAME := aosp_phoenix
+# Device identifier
+PRODUCT_NAME := lineage_phoenix
 PRODUCT_DEVICE := phoenix
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi K30
