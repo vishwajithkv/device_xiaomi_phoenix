c28e792e6a2555a1f415d5397277b7eeabd96bec
mikeNG <mikeNG@users.noreply.github.com>
2024-09-18 08:59:46 +0000

    phoenix: Sync extract-files scripts with latest templates

--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# Copyright (C) 2024 The LineageOS Project
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -15,6 +16,10 @@ MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}/../../.."
+
+# If XML files don't have comments before the XML header, use this flag
+# Can still be used with broken XML files by using blob_fixup
+export TARGET_DISABLE_XML_FIXING=true
 
 export TARGET_ENABLE_CHECKELF=true
 
@@ -40,7 +45,8 @@ while [ "${#}" -gt 0 ]; do
                 KANG="--kang"
                 ;;
         -s | --section )
-                SECTION="${2}"; shift
+                SECTION="${2}"
+                shift
                 CLEAN_VENDOR=false
                 ;;
         * )
@@ -57,25 +63,42 @@ fi
 function blob_fixup() {
     case "${1}" in
         vendor/etc/init/init.mi_thermald.rc)
+        [ "$2" = "" ] && return 0
             sed -i "/seclabel u:r:mi_thermald:s0/d" "${2}"
             ;;
         vendor/lib64/libgoodixhwfingerprint.so )
+        [ "$2" = "" ] && return 0
         "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
             ;;
         vendor/lib64/camera/components/com.qti.node.watermark.so )
+        [ "$2" = "" ] && return 0
         grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
         vendor/lib64/hw/camera.qcom.so | vendor/lib64/libFaceDetectpp-0.5.2.so | vendor/lib64/libfacedet.so)
+        [ "$2" = "" ] && return 0
             sed -i "s|libmegface.so|libfacedet.so|g" "${2}"
             sed -i "s|libMegviiFacepp-0.5.2.so|libFaceDetectpp-0.5.2.so|g" "${2}"
             sed -i "s|megviifacepp_0_5_2_model|facedetectpp_0_5_2_model|g" "${2}"
-            ;;    
+            ;;  
+            *)
+                return 1
+                ;;  
     esac
+
+  return 0
+}
+
+function blob_fixup_dry() {
+    blob_fixup "$1" ""
 }
 
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
 
 extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
+
+if [ -z "${SECTION}" ]; then
+    extract_firmware "${MY_DIR}/proprietary-firmware.txt" "${SRC}"
+fi
 
 "${MY_DIR}/setup-makefiles.sh"
